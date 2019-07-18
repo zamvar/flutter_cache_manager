855f306104066db93812ff7217f6b14c455625ae
Rene Floor <r.floor.1@gmail.com>
2019-07-18 21:47:55 +0200

    Return error as FIleInfo object

--- lib/src/file_info.dart
+++ lib/src/file_info.dart
@@ -8,9 +8,17 @@ enum FileSource { NA, Cache, Online }
 
 class FileInfo {
   FileInfo(this.file, this.source, this.validTill, this.originalUrl);
+  FileInfo.withError(this.originalUrl, this.error);
 
   String originalUrl;
   File file;
   FileSource source;
   DateTime validTill;
+  Object error;
+
+  /// Returns whether this snapshot contains a non-null [error] value.
+  ///
+  /// This is always true if the asynchronous computation's last result was
+  /// failure.
+  bool get hasError => error != null;
 }
